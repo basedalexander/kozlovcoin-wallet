fd559ef2b599e0b3c8240736092fbc8a12c500aa
thohoh <itrefak@gmail.com>
2018-02-12 11:02:02 +0200

    #1 Cleanup

--- e2e/app.e2e-spec.ts
+++
@@ -1,9 +0,0 @@
-import { AngularNgrxMaterialStarterPage } from './app.po';
-
-describe('angular-ngrx-material-starter App', () => {
-  let page: AngularNgrxMaterialStarterPage;
-
-  beforeEach(() => {
-    page = new AngularNgrxMaterialStarterPage();
-  });
-});

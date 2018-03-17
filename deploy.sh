997555a1ad1f13d423a6b0f5d1d1aa0d7211a84a
thohoh <itrefak@gmail.com>
2018-03-17 02:45:23 +0200

    Replaced travis ci with custom deployment script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+KEY_PATH=~/Dropbox/AWS_keys/ec2-1.pem
+EC2_USER=ec2-user
+REMOTE_HOST=ec2
+TAR_NAME=kozlovcoin-wallet.tar.gz
+
+npm run build:prod
+tar -czf $TAR_NAME -C dist .
+scp -i $KEY_PATH $TAR_NAME $EC2_USER@$REMOTE_HOST:~/thohoh.pro/$TAR_NAME
+rm -rf ./$TAR_NAME
+
+ssh -i $KEY_PATH $EC2_USER@$REMOTE_HOST << 'ENDSSH'
+cd thohoh.pro
+rm -rf wallet_public
+mkdir wallet_public
+tar -xvf kozlovcoin-wallet.tar.gz -C wallet_public
+rm kozlovcoin-wallet.tar.gz
+exit
